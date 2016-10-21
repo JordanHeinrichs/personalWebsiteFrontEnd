e94ff97a17a0b613bea21d1fad4d33d86e3abfcc
Jordan Heinrichs <mail@jordanheinrichs.com>
2016-10-21 01:57:12 -0600

    Added routing in to blog-content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,9 @@ export const ROUTES: Routes = [
    { path: '',      component: Home },
    { path: 'home',  component: Home },
    { path: 'projects', component: Projects },
+   { path: 'projects/:name', component: Projects },
    { path: 'hobbies', component: Hobbies },
+   { path: 'hobbies/:name', component: Hobbies },
    { path: 'contact-me', component: ContactMe },
    { path: '**',    component: NoContent },
 ];
--- src/app/blog-content/blog-content.component.ts
+++ src/app/blog-content/blog-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit  } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ItemSelector, ItemChoice } from './../itemselector';
 import { BlogContentService } from './blog-content.service';
 import { BlogUrlService } from './blog-url-route.service';
@@ -11,22 +12,38 @@ import { BlogUrlService } from './blog-url-route.service';
 export class BlogContent implements OnInit {
    blogList: ItemChoice[];
    blogHtml: string;
+   blogNameParam: string;
 
-   constructor(private blogContentService: BlogContentService) {
+   constructor(private blogContentService: BlogContentService, 
+      private router: Router,
+      private route: ActivatedRoute) {
       this.blogList = null;
       this.blogHtml = '';
+      this.blogNameParam = null;
    }
 
    ngOnInit(): void {
+      this.route.params.forEach((params: Params) => {
+         this.blogNameParam = params['name'];
+         if (this.blogNameParam) {
+            const item: ItemChoice = {name: this.blogNameParam, src: null};
+            this.onChoiceSelected(item);
+         }
+      });
+
       this.blogContentService.blogList()
          .then(blogList => this.blogList = blogList)
          .then(blogList => {
-            // Always load the first blog
-            this.onChoiceSelected(blogList[0]);
+            if (!this.blogNameParam) {
+               // Always load the first blog if no parameter is set
+               this.onChoiceSelected(blogList[0]);
+            }
          });
    }
 
    onChoiceSelected(item: ItemChoice) {
+      this.blogNameParam ? this.router.navigate(['../', item.name], { relativeTo: this.route }) :
+         this.router.navigate([item.name], { relativeTo: this.route });
       this.blogContentService.blog(item.name)
          .then(blog => this.blogHtml = blog)
          .catch((error: any) => this.blogHtml = 'Error: can not load blog.' );
